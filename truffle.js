23a47dd3c751e3ba6a6b9b425fd6ec44473bcb81
Nikolai <nikolai@Nikolais-MacBook-Air.local>
2017-12-21 11:00:28 +0100

    first commit

---
+++ truffle.js
@@ -0,0 +1,20 @@
+// Allows us to use ES6 in our migrations and tests.
+require('babel-register')
+
+module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id,
+      gas: 4612388 // Gas limit used for deploys
+    },
+    rinkeby: {
+      host: "localhost", // Connect to geth on the specified
+      port: 8545,
+      from: "0x2df219a1ae3845ab32a041499db27d4bfdb43876", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 4612388 // Gas limit used for deploys
+    }
+  }
+};
